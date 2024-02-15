78cb6aeb39000ac3e359b9cdbe93978163007dca
ShashankKajla <shashankkajla.sk@gmail.com>
2024-02-15 19:36:04 +0530

    chrome isue resolved

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -61,7 +61,8 @@ export const config: Options.Testrunner = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        browserVersion: '122.0.6261.39'
     }],
 
     //
@@ -111,7 +112,7 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    // services: ['chromedriver'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
